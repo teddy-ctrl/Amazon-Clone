3575a0857224dde1f8c890a112741cf94ff631df
teddy-ctrl <tedu21@gmail.com>
2025-05-12 21:22:40 +0300

    Header NavBarList

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar/NavBar'
+import NavBarList from './NavBarList/NavBarList'
 
 const Header = () => {
   return (
     <>
       <NavBar />
+      <NavBarList />
     </>
   )
 }
--- src/Components/Header/NavBarList/NavBarList.jsx
+++ src/Components/Header/NavBarList/NavBarList.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import classes from './navbarlist.module.css'
+import { MdMenu } from "react-icons/md";
 
 const NavBarList = () => {
+ const options = [
+    { name: "Today`s Deals" },
+    { name: "Prime Video" },
+    { name: "Registry" },
+    { name: "Customer Service" },
+    { name: "Fift Cards" },
+    { name: "Sell" },
+  ];
   return (
     <>
-      
+      <div className={classes.navbarBanner}>
+        <div className={classes.nabarBann}>
+          <div className={classes.optionsNavbar}>
+            <MdMenu sx={{ fontSize: "24px" }} />
+            <div className={classes.allOptionsNavbar}>All</div>
+          </div>
+
+          {
+            options.map((item, ind) => {
+              return (
+                <div className={classes.optionsNavbar} key={ind}>
+                  <div className={classes.allOptionsNavbar}>{item.name}</div>
+                </div>
+              );
+            })
+          }
+
+        
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default NavBarList
